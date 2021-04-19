859ff1a10d567b98b6298f981b2bbcbb5e61f05f
shivavaddagani <66823848+shivavaddagani@users.noreply.github.com>
2021-04-19 13:07:28 +0530

    Practising how to create a package created two modules and called them

--- cars/first.py
+++ cars/first.py
@@ -1,3 +1,4 @@
-def say_hi():
+def say_hi():            # this is my first module. This is similar to importing a module(barplot) from matplotlib.pyplot
         print("hi")
-        
+def say_bye():           # this is my second module. This is similar to importing a module(histogram) from matplotlib.pyplot
+        print("bye")
--- code.py
+++ code.py
@@ -1,2 +1,3 @@
-from cars import first
-print(first.say_hi())
+from cars import first              # importing the first module from cars package
+print(first.say_hi())               # running a function from first module
+print(first.say_bye())
